a14d9df61313eb3ec0b5bd495e5c0336ae9a0d2d
Achim <cadarachim@gmail.com>
2024-05-22 14:15:34 +0300

    forth exercice

---
+++ asii/asii.py
@@ -0,0 +1,7 @@
+number_one = int(input("enter a number:"))
+number_two = int(input("enter a number:"))
+sum = number_one + number_two
+dif = number_one - number_two
+prod = number_one * number_two
+cat = number_one / number_two
+print(sum, dif, prod, cat)
