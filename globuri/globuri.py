fa7445cc3e77a36fd915d9e27c0a920d01ae9b78
Achim <cadarachim@gmail.com>
2024-05-30 12:48:59 +0300

    globuri

---
+++ globuri/globuri.py
@@ -0,0 +1,6 @@
+globuri_albe = int(input("introdu nr de globuri: "))
+globuri_rosii = globuri_albe * 2
+globuri_verzi = globuri_rosii - 3
+total_globuri = globuri_albe + globuri_rosii + globuri_verzi
+
+print(total_globuri)
